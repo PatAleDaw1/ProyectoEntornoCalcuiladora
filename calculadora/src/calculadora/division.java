022635aaddabd75a828f17dbdb7053d38b7655f9
laurasoni <laura.martinezh90@gmail.com>
2017-11-22 18:31:03 +0100

    division

---
+++ calculadora/src/calculadora/division.java
@@ -0,0 +1,18 @@
+package division;
+import java.util.*;
+public class division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2, division;
+		Scanner teclado= new Scanner(System.in);
+		System.out.println("num1");
+		num1= teclado.nextInt();
+		System.out.println("num2");
+		num2= teclado.nextInt();
+		division=num1/num2;
+		System.out.println(division);
+	}//main
+	
+
+}//class
